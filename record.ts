ead7f66821e77d8f79f9fd258f24e84f855299bf
Simon Fessehaye <fessehay@ualberta.ca>
2019-10-07 22:19:40 -0600

    made some progress...

--- record.ts
+++ record.ts
@@ -5,13 +5,44 @@ import { join } from 'path';
 import { exec,execSync } from 'child_process';
 const { default: SlippiGame } = require('slp-parser-js');
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const obs = new OBSWebSocket();
+const DIR = process.env.DIR;
 
 
+async function record(folder):Promise<void> {
+    try {
+        const files = fs.readdirSync(`${DIR}\\${folder}`).filter(file => file.includes("slp"));
+        const totalSeconds = files.reduce((acc,curr) => {
+            const game = new SlippiGame(`${DIR}\\${folder}\\${curr}`);
+            const count = Math.ceil(game.getMetadata().lastFrame/60);
+            return acc + count;
+        },0);
+                
+        console.log(`${folder} is being played`);
+        const file = `"${DIR}\\${folder}\\start.bat"`;
+        
+        await obs.send('SetRecordingFolder', {
+            'rec-folder': `${DIR}\\${folder}\\`
+        });
+
+        exec(file);
+        console.log("running dolphin");
+        await obs.send('StartStopRecording');
+
+        // wait 5 additional seconds
+        console.log(`Waiting ${totalSeconds+10} seconds`);
+
+        await sleep((totalSeconds+10)*1000);
+        await obs.send('StartStopRecording');
+        execSync('taskkill /F /IM "Dolphin.exe" /T');
+        return Promise.resolve();
+    } catch (error) {
+        console.log(error);
+    }
+}
 (async function(){
     try {
-        
-        const obs = new OBSWebSocket();
-        
+
         await obs.connect({ address: 'localhost:4444',password: 'slippi' });
 
         console.log('Connection Opened');
@@ -20,26 +51,14 @@ const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
             'scene-name': 'Slippi'
         });
 
-        const DIR = process.env.DIR;
-
         const folders = fs.readdirSync(DIR).filter(f => fs.statSync(join(DIR, f)).isDirectory());
 
-        folders.forEach( async (folder) => {
-            const files = fs.readdirSync(`${DIR}\\${folder}`).filter(file => file.includes("slp"));
-            const totalSeconds = files.reduce((acc,curr) => {
-                const game = new SlippiGame(`${DIR}\\${folder}\\${curr}`);
-                const count = Math.ceil(game.getMetadata().lastFrame/60);
-                return acc + count;
-            },0);
-            console.log("calc files");
-            const file = `"${DIR}\\${folders}\\start.bat"`;
-           
-            execSync(file);
-            console.log("running dolpin");
-            await sleep((totalSeconds+30)*1000);
-            execSync('taskkill /F /IM "Dolphin.exe" /T');
-
-        })
+        const recordTasks = folders.reduce((tasks,folder,index) => {
+            return tasks.then(async () => {
+                return record(folder);
+            });
+        },Promise.resolve());
+        
     }
     catch (error) {
         console.error(error);
